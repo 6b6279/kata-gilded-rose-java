6bec4a017536b0de0cdd9668045eaa42eab75aff
Kaan B. Y. <109346960+6b6279@users.noreply.github.com>
2026-06-25 11:09:35 +0000

    Commit changes

--- diff_golden_master.sh
+++ diff_golden_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn compile exec:java --quiet | head -n -1 > golden.tmp
+mvn compile test-compile exec:java --quiet | head -n -1 > golden.tmp
 diff -y --color=always --left-column goldenMasterTemplate.txt <(cat golden.tmp)
 echo ""
 diff -sq goldenMasterTemplate.txt <(cat golden.tmp)
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,12 @@
 package com.gildedrose;
 
 class GildedRose {
+
+    private final static String SPECIAL_ITEM_NAME_SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private final static String SPECIAL_ITEM_NAME_AGED_BRIE = "Aged Brie";
+    private final static String SPECIAL_ITEM_NAME_80ETC_BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private final static int CURRENT_EXPANSION_QUALITY_CAP = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,54 +15,48 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            if (items[i].name.equals(SPECIAL_ITEM_NAME_SULFURAS)) {
+                // Legendary items don't lose quality over time...
+                // By fire be purged!
+                continue;
+            }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(SPECIAL_ITEM_NAME_AGED_BRIE)
+                    && !items[i].name.equals(SPECIAL_ITEM_NAME_80ETC_BACKSTAGE_PASS)) {
+                    modifyQuality(items[i], -1);
+            } else {
+                modifyQuality(items[i], 1);
+                if (items[i].name.equals(SPECIAL_ITEM_NAME_80ETC_BACKSTAGE_PASS)) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            modifyQuality(items[i], 1);
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            modifyQuality(items[i], 1);
                         }
                     }
-                }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn--;
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
+                if (items[i].name.equals(SPECIAL_ITEM_NAME_AGED_BRIE)) {
+                    modifyQuality(items[i], 1);
+                } else if (items[i].name.equals(SPECIAL_ITEM_NAME_80ETC_BACKSTAGE_PASS)) {
+                    // You missed the concert!
+                    modifyQuality(items[i], -50);
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    modifyQuality(items[i], -1);
                 }
             }
         }
+    }
+
+    private void modifyQuality(Item item, int steps) {
+        if (item == null) {
+            return;
+        }
+
+        item.quality = Math.clamp(item.quality + steps, 0, CURRENT_EXPANSION_QUALITY_CAP);
     }
 }
